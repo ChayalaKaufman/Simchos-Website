fee821be8bcb8f2847cbe5c25dd847890b81cb06
Chayala Kaufman <chayalakaufman@gmail.com>
2019-03-26 09:12:13 -0400

    Add project files.

---
+++ homework 032519 SimchosAndContributors/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace homework_032519_SimchosAndContributors
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
